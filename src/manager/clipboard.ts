d673b0f543106c4084d56ca91ba4f226960963c0
liyongjin <liyongjin2020@126.com>
2023-07-24 08:35:11 +0800

    refactor: delete clipboardy module

--- src/manager/clipboard.ts
+++ src/manager/clipboard.ts
@@ -1,95 +1,53 @@
-import * as clipboardy from "clipboardy";
 import * as vscode from "vscode";
 
 /**
  * Clipboard base class to read and write text and detect changes
  */
 export abstract class BaseClipboard {
-  protected _disposables: vscode.Disposable[] = [];
+    protected _disposables: vscode.Disposable[] = [];
 
-  private _onDidWillWriteText = new vscode.EventEmitter<string>();
-  public readonly onDidWillWriteText = this._onDidWillWriteText.event;
+    private _onDidWillWriteText = new vscode.EventEmitter<string>();
+    public readonly onDidWillWriteText = this._onDidWillWriteText.event;
 
-  private _onDidWriteText = new vscode.EventEmitter<string>();
-  public readonly onDidWriteText = this._onDidWriteText.event;
+    private _onDidWriteText = new vscode.EventEmitter<string>();
+    public readonly onDidWriteText = this._onDidWriteText.event;
 
-  constructor() {
-    this._disposables.push(this._onDidWillWriteText);
-    this._disposables.push(this._onDidWriteText);
-  }
+    constructor() {
+        this._disposables.push(this._onDidWillWriteText);
+        this._disposables.push(this._onDidWriteText);
+    }
 
-  public readText(): Thenable<string> {
-    return this.readTextInternal();
-  }
+    public readText(): Thenable<string> {
+        return this.readTextInternal();
+    }
 
-  public async writeText(value: string) {
-    this._onDidWillWriteText.fire(value);
+    public async writeText(value: string) {
+        this._onDidWillWriteText.fire(value);
 
-    await this.writeTextInternal(value);
+        await this.writeTextInternal(value);
 
-    this._onDidWriteText.fire(value);
-  }
+        this._onDidWriteText.fire(value);
+    }
 
-  protected abstract readTextInternal(): Thenable<string>;
-  protected abstract writeTextInternal(value: string): Thenable<void>;
+    protected abstract readTextInternal(): Thenable<string>;
+    protected abstract writeTextInternal(value: string): Thenable<void>;
 
-  public dispose() {
-    this._disposables.forEach(d => d.dispose());
-  }
+    public dispose() {
+        this._disposables.forEach(d => d.dispose());
+    }
 }
 
 export class VSCodeClipboard extends BaseClipboard {
-  protected readTextInternal(): Thenable<string> {
-    return vscode.env.clipboard.readText();
-  }
-  protected writeTextInternal(value: string): Thenable<void> {
-    return vscode.env.clipboard.writeText(value);
-  }
-}
-
-export class ClipboardyClipboard extends BaseClipboard {
-  protected readTextInternal(): Thenable<string> {
-    let promise = clipboardy.read();
-
-    /**
-     * Fix problem in `clipboardy` when clipboard text is empty on windows
-     * Example: After power up or after a print screen
-     */
-    if (process.platform === "win32") {
-      promise = promise.then(null, (reason: any) => {
-        const ignoreMessage =
-          "thread 'main' panicked at 'Error: Could not paste from clipboard: Error { repr: Os { code: 0, message:";
-
-        if (reason.stderr && reason.stderr.startsWith(ignoreMessage)) {
-          // return empty content
-          return "";
-        }
-
-        throw reason;
-      });
+    protected readTextInternal(): Thenable<string> {
+        return vscode.env.clipboard.readText();
     }
-
-    return promise;
-  }
-  protected writeTextInternal(value: string): Thenable<void> {
-    return clipboardy.write(value);
-  }
+    protected writeTextInternal(value: string): Thenable<void> {
+        return vscode.env.clipboard.writeText(value);
+    }
 }
 
 export function getNewDefaultInstance() {
-  let clipboard;
-
-  try {
-    vscode.env.clipboard.readText();
-    clipboard = new VSCodeClipboard();
-    // tslint:disable-next-line:no-empty
-  } catch (error) {}
-
-  if (!clipboard) {
-    clipboard = new ClipboardyClipboard();
-  }
-
-  return clipboard;
+    return new VSCodeClipboard();
 }
 
 export const defaultClipboard: BaseClipboard = getNewDefaultInstance();
