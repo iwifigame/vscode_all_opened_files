9fbd06fbb5d09b39ae892f5b539cc8ecb0e27d89
liyongjin <liyongjin2020@126.com>
2023-12-15 09:38:14 +0800

    feat: auto highlight current bookmark

--- src/tree/bookmarkTree.ts
+++ src/tree/bookmarkTree.ts
@@ -3,7 +3,7 @@ import * as vscode from 'vscode';
 import { commandList } from '../global';
 import { AbstractManager } from '../manager/abstractManager';
 import { IFileTextItem } from '../manager/common';
-import { createIconPath, pathEqual } from '../util/util';
+import { createIconPath, getWordAtCursor, pathEqual } from '../util/util';
 
 export class BookmarkItem extends vscode.TreeItem {
     constructor(readonly bookmark: IFileTextItem) {
@@ -21,6 +21,7 @@ export class BookmarkItem extends vscode.TreeItem {
             arguments: [null, this.bookmark],
         };
 
+        // 从左到右：文件后缀对应的图标  label description(将暗，缩小显示)
         if (this.bookmark.createdLocation) {
             this.resourceUri = this.bookmark.createdLocation.uri; // 会自动根据后缀设置前面的图标
             this.contextValue += 'file';
@@ -52,6 +53,20 @@ export class BookmarkTreeDataProvider
             this._onDidChangeTreeData.fire(null); // manager的事件修改了，这里也派发事件
         });
 
+        // 选择范围变化时，自动选择当前光标下的书签
+        vscode.window.onDidChangeTextEditorSelection((e) => {
+            let editor = e.textEditor;
+
+            let doc = editor.document;
+            if (doc == undefined) {
+                return;
+            }
+
+            let filePath = doc.fileName;
+            let text = getWordAtCursor(editor);
+            this.autoSelectCurrentFileItems(filePath, text);
+        });
+
         vscode.window.onDidChangeActiveTextEditor((editor) => {
             if (!this.tree || !this.tree.visible) {
                 return;
@@ -67,8 +82,10 @@ export class BookmarkTreeDataProvider
             }
 
             let filePath = doc.fileName;
-            // 这里拿不到当前光标下所在的单词。就无法将bookmart树，高亮指定的书签。只能随便选一个
-            this.autoSelectCurrentFileItems(filePath);
+            // todo: 怎么拿不到光标下的单词？这里总是拿到第一个单词。原因未知
+            let text = getWordAtCursor(editor);
+            // log('光标下的单词', text);
+            this.autoSelectCurrentFileItems(filePath, text);
         });
     }
 
@@ -76,34 +93,46 @@ export class BookmarkTreeDataProvider
         this.tree = t;
     }
 
-    private autoSelectCurrentFileItems(filePath: string) {
+    private autoSelectCurrentFileItems(filePath: string, text: string) {
         if (!this.tree || !this.tree.visible) {
             return;
         }
 
-        // 如果当前树中选择的路径和指定路径相同，则不处理
+        // 当前树中选择的路径是否和指定路径相同
+        let isAtCurrentFile = false;
         if (this.tree.selection.length > 0) {
             let cur = this.tree.selection[0];
             if (pathEqual(cur.bookmark.createdLocation?.uri.path, filePath)) {
-                return;
+                isAtCurrentFile = true;
             }
         }
 
-        // 高亮显示当前文件中的标签
-        const item = this.getOneTreeItemByPath(filePath);
-        if (!item) {
-            return;
-        }
-        this.tree.reveal(item, { focus: false, select: true });
-    }
-
-    private getOneTreeItemByPath(filePath: string) {
+        let targetItem;
+        let firstItem;
         for (var i = this.data.length - 1; i >= 0; i--) {
             let x = this.data[i];
             if (pathEqual(x.bookmark.createdLocation?.uri.path, filePath)) {
-                return x;
+                if (!firstItem) {
+                    firstItem = x;
+                }
+                if (x.bookmark.value == text) {
+                    targetItem = x;
+                    break;
+                }
             }
         }
+
+        // 不是同一个文件。并且没有找到相同的，取第一个
+        if (!isAtCurrentFile && !targetItem) {
+            targetItem = firstItem;
+        }
+
+        if (!targetItem) {
+            return;
+        }
+
+        // 高亮显示找到的书签
+        this.tree.reveal(targetItem, { focus: false, select: true });
     }
 
     getParent(element: BookmarkItem): vscode.ProviderResult<BookmarkItem> {
@@ -130,7 +159,7 @@ export class BookmarkTreeDataProvider
                 item.label = `${c.param}) ${item.label}`;
             } else {
                 // 普通书签
-                item.label = `${indexNumber}) ${item.label}`;
+                item.label = `${indexNumber}) ${item.label}`; // 序号)书签名
             }
 
             return item;
